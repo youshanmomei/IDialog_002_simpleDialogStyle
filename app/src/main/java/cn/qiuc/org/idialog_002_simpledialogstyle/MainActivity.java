992082d3b6b1987fa395671c74a7409622649183
qc <1142041743@qq.com>
2016-06-25 22:22:25 +0800

    Initial commit -Dialog的style

---
+++ app/src/main/java/cn/qiuc/org/idialog_002_simpledialogstyle/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.qiuc.org.idialog_002_simpledialogstyle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
