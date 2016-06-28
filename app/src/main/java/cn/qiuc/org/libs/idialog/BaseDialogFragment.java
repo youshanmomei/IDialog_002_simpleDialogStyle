7bd72169663ff1feb00b875bbafec37fe8f1c59d
qc <1142041743@qq.com>
2016-06-28 13:05:19 +0800

    1.R.style.SDL_Dialog 2.R.styleable.DialogStyle 3.R.attr.sdlDialogStyle

--- app/src/main/java/cn/qiuc/org/libs/idialog/BaseDialogFragment.java
+++ app/src/main/java/cn/qiuc/org/libs/idialog/BaseDialogFragment.java
@@ -1,9 +1,24 @@
 package cn.qiuc.org.libs.idialog;
 
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.os.Bundle;
+
+import cn.qiuc.org.idialog_002_simpledialogstyle.R;
 
 /**
  * Created by admin on 2016/6/26.
  */
 public class BaseDialogFragment extends DialogFragment{
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = new Dialog(getActivity(), R.style.SDL_Dialog);
+
+        //dialog background
+        getActivity().getTheme().obtainStyledAttributes(null, R.styleable.DialogStyle, R.attr.sdlDialogStyle, 0);
+
+
+        return dialog;
+    }
 }
